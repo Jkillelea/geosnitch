b2605c4d04b2a699b507483437063bc651e34408
Jacob Killelea <Jkillelea@protonmail.ch>
2018-02-25 23:37:46 -0700

    move MQTT functionality to own thread

--- src/main.rs
+++ src/main.rs
@@ -11,48 +11,40 @@ extern crate dbus;
 use dbus::*;
 
 extern crate mqtt;
-use mqtt::{Decodable, Encodable};
-use mqtt::packet::*;
-use mqtt::TopicName;
+use mqtt::{Encodable};
 use mqtt::control::variable_header::ConnectReturnCode;
 
+mod mqtt_session;
+use mqtt_session::MqttSession;
 mod mqtt_config;
 mod location;
 use location::Location;
 
 fn main() {
     let c = Connection::get_private(BusType::Session).unwrap(); // D-Bus init
-    println!("provider: {}", location::provider_name(&c));
-    println!("location: {}", location::addr_provider_name(&c));
-    println!("position: {}", location::position_provider_name(&c));
+    println!("---");
+    println!("master provider: {}", location::provider_name(&c));
+    println!("location provider: {}", location::addr_provider_name(&c));
+    println!("position provider: {}", location::position_provider_name(&c));
+    println!("---");
 
-    let mut buf    = Vec::new();
-    let     conf   = mqtt_config::get("mqtt-auth.json"); // not in version control
-    let mut stream = TcpStream::connect(conf.server).unwrap(); // MQTT init
-    let mut conn   = ConnectPacket::new("MQTT", conf.client_id);
-    conn.set_clean_session(true);
-    conn.set_user_name(conf.username);
-    conn.set_password(conf.password);
-    conn.encode(&mut buf).unwrap();
-    stream.write_all(&buf).unwrap();
+    let conf = mqtt_config::get("mqtt-auth.json"); // this file is not in version control
+    let mut session = MqttSession::new(conf.server);
+    session.connect(conf.client_id, conf.username, conf.password);
 
-    let ack = ConnackPacket::decode(&mut stream).unwrap();
+    let ack = session.read_ack().unwrap();
     if ack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
         panic!("Failed to connect to server, return code {:?}", ack.connect_return_code());
     }
 
     loop { // send location periodically
-        buf.clear();
-        let location = Location::get_address(&c).unwrap_or(Location::empty())
-                                .merge(Location::get_position(&c).unwrap_or(Location::empty()))
+        let location = Location::get_address(&c)
+                                .unwrap_or(Location::empty())
+                                .merge(Location::get_position(&c)
+                                .unwrap_or(Location::empty()))
                                 .as_json().unwrap();
         println!("{}", location);
-
-        let publish = PublishPacket::new(TopicName::new(conf.topic.clone()).unwrap(),
-                                    QoSWithPacketIdentifier::Level0,
-                                    location);
-        publish.encode(&mut buf).unwrap();
-        stream.write_all(&buf).unwrap();
-        thread::sleep(Duration::from_secs(10));
+        session.publish(conf.topic.clone(), location);
+        thread::sleep(Duration::from_secs(conf.send_period.unwrap_or(10)));
     }
 }
---
+++ src/mqtt_session.rs
@@ -0,0 +1,46 @@
+use std::io::prelude::*;
+use std::net::{TcpStream, ToSocketAddrs};
+
+use mqtt;
+use mqtt::{Encodable, Decodable};
+use mqtt::TopicName;
+use mqtt::packet::*;
+
+pub struct MqttSession {
+    stream: TcpStream,
+}
+
+impl MqttSession {
+    pub fn new<Addr: ToSocketAddrs>(server: Addr) -> MqttSession {
+        MqttSession {
+            stream: TcpStream::connect(server).unwrap(),
+        }
+    }
+
+    pub fn connect(&mut self, client_id: String, username: Option<String>, password: Option<String>) {
+        let mut conn = ConnectPacket::new("MQTT", client_id);
+        conn.set_clean_session(true);
+        conn.set_user_name(username);
+        conn.set_password(password);
+        send(&conn, &mut self.stream);
+    }
+
+    pub fn publish<Data: Into<Vec<u8>>>(&mut self, topic: String, data: Data) {
+        let publish = PublishPacket::new(TopicName::new(topic).unwrap(),
+                                        QoSWithPacketIdentifier::Level0,
+                                        data);
+        send(&publish, &mut self.stream);
+    }
+
+    pub fn read_ack(&mut self)
+        -> Result<ConnackPacket, mqtt::packet::PacketError<mqtt::packet::ConnackPacket>> {
+        ConnackPacket::decode(&mut self.stream)
+    }
+}
+
+
+fn send<'a, Pack: Encodable<'a>>(packet: &'a Pack, stream: &mut TcpStream) {
+    let mut buf = Vec::new();
+    packet.encode(&mut buf).unwrap();
+    stream.write_all(&buf).unwrap();
+}
