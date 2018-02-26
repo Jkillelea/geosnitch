10f3d50ea3d561cae0001e7daa026872523ed1a5
Jacob Killelea <Jkillelea@protonmail.ch>
2018-02-25 23:39:54 -0700

    add send_period field

--- src/mqtt_config.rs
+++ src/mqtt_config.rs
@@ -6,11 +6,12 @@ use serde_json;
 
 #[derive(Debug, Deserialize)]
 pub struct MqttConfig {
-    pub server: String,
-    pub username: Option<String>,
-    pub password: Option<String>,
-    pub client_id: String,
-    pub topic: String,
+    pub server: String,           // server name:port
+    pub username: Option<String>, // login username
+    pub password: Option<String>, // login password
+    pub client_id: String,        // MQTT client ID
+    pub topic: String,            // MQTT topic
+    pub send_period: Option<u64>, // send every $(send_period) seconds
 }
 
 /* 'path' should point to a file that looks like:
