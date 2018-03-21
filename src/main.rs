1b769dcaaaeaf5b07d372712dbcc9a06e4f3d4a0
Jacob Killelea <Jkillelea@protonmail.ch>
2018-03-21 00:58:05 -0600

    misc experimentations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::thread;
 use std::time::Duration;
 
+#[macro_use]
+extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
-#[macro_use] extern crate serde_derive;
 
 extern crate dbus;
 use dbus::*;
@@ -12,27 +13,34 @@ extern crate mqtt;
 use mqtt::control::variable_header::ConnectReturnCode;
 
 mod mqtt_session;
-use mqtt_session::MqttSession;
 mod mqtt_config;
 mod location;
+use mqtt_session::MqttSession;
 use location::Location;
 
 fn main() {
     let c = Connection::get_private(BusType::Session).unwrap(); // D-Bus init
+
+    let providers = ["Geonames", "Localnet", "Skyhook", "UbuntuGeoIP"];
+    for p in providers.iter() {
+        let dest = format!("org.freedesktop.Geoclue.Providers.{}", p);
+        let path = format!("/org/freedesktop/Geoclue/Providers/{}", p);
+        println!("{}: {:#?}", p, location::provider_status(&c, &dest, &path));
+    }
+
     println!("---");
+    println!("clients: {:?}", location::available_clients(&c));
     println!("master provider: {}",   location::provider_name(&c));
     println!("location provider: {}", location::addr_provider_name(&c));
     println!("position provider: {}", location::position_provider_name(&c));
     println!("---");
 
-    let conf = mqtt_config::get("mqtt-auth.json"); // this file is not in version control
-    let mut session = MqttSession::new(conf.server);
-    session.connect(conf.client_id, conf.username, conf.password);
+    let conf = mqtt_config::get(&"mqtt-auth.json"); // this file is not in version control
+    let mut mqtt_session = MqttSession::new(conf.server);
+    mqtt_session.connect(conf.client_id, conf.username, conf.password);
 
-    let ack = session.read_ack().unwrap();
-    if ack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
-        panic!("Failed to connect to server, return code {:?}", ack.connect_return_code());
-    }
+    let ack = mqtt_session.read_ack().unwrap();
+    assert_eq!(ack.connect_return_code(), ConnectReturnCode::ConnectionAccepted);
 
     loop { // send location periodically
         let location = Location::get_address(&c)
@@ -41,7 +49,9 @@ fn main() {
                                 .unwrap_or(Location::empty()))
                                 .as_json().unwrap();
         println!("{}", location);
-        session.publish(conf.topic.clone(), location);
+        mqtt_session.publish(conf.topic.clone(), location);
         thread::sleep(Duration::from_secs(conf.send_period.unwrap_or(10)));
     }
 }
+
+
